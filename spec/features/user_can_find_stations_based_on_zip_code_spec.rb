b1fe17c72cc6d4fbb1e9eee7cf360b73dfbddca5
VictoriaVasys <vvasys@gmail.com>
2017-05-17 10:00:42 -0600

    Complete feature test

--- spec/features/user_can_find_stations_based_on_zip_code_spec.rb
+++ spec/features/user_can_find_stations_based_on_zip_code_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 feature 'User sees electric & propane stations within 6 miles of zip code' do
-  scenario 'User is navigated to correct page' do
+  scenario 'User is navigated to correct page and sees station information' do
     visit root_path
     fill_in "Search by zip...", with: "80203"
     click_on "Locate"
     
     expect(current_path).to eq search_path
-  end
-  
-  scenario 'User sees station information' do
-    visit sear
+    within('station_1') {expect(page).to have_content "Name: UDR"}
+    within('station_1') {expect(page).to have_content "Address: 800 Acoma St, Denver, CO 80204"}
+    within('station_1') {expect(page).to have_content "Fuel Type: Electric"}
+    within('station_1') {expect(page).to have_content "Distance: 0.314 miles"}
+    within('station_1') {expect(page).to have_content "Access Times: 24 hours daily"}
   end
 end
