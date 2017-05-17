1973cfacbcf59a29b233161fce18f41242f6ec78
VictoriaVasys <vvasys@gmail.com>
2017-05-17 11:00:52 -0600

    Build NREL Service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,7 @@ class SearchController < ApplicationController
   
   def index
     @zip = params['q']
-    conn = Faraday.new("https://developer.nrel.gov")
-    response = conn.get do |req|
-      req.url '/api/alt-fuel-stations/v1/nearest.json'
-      req.params['location'] = @zip
-      req.params['radius'] = 6.0
-      req.params['fuel_type'] = 'LPG,ELEC'
-      req.params['limit'] = 10
-      req.params['api_key'] = ENV['NREL_KEY']
-    end
+    
     @stations = (JSON.parse(response.body, symbolize_names: true))[:fuel_stations]
   end
   
