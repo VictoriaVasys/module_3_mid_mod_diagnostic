b1738ac6523577763133953851b79838c659dc1e
VictoriaVasys <vvasys@gmail.com>
2017-05-17 10:32:03 -0600

    Create Faraday request and begin search index view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,17 @@
 class SearchController < ApplicationController
   
   def index
-    conn = Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json")
-    response = 
-    @stations = 
+    @zip = params['q']
+    conn = Faraday.new("https://developer.nrel.gov")
+    response = conn.get do |req|
+      req.url '/api/alt-fuel-stations/v1/nearest.json'
+      req.params['location'] = zip
+      req.params['radius'] = 6.0
+      req.params['fuel_type'] = 'LPG,ELEC'
+      req.params['limit'] = 10
+      req.params['api_key'] = ENV['NREL_KEY']
+    end
+    @stations = JSON.parse(response.body)
   end
   
 end
